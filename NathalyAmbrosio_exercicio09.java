4356a25e7143a3c4c342e7773ba2b5f4111a6e99
NathalyAmbrosio <131813980+NathalyAmbrosio@users.noreply.github.com>
2023-05-23 13:55:42 -0300

    Update NathalyAmbrosio_exercicio09.java
    add

---
+++ NathalyAmbrosio_exercicio09.java
@@ -0,0 +1,34 @@
+/*
+Algoritmo que compara os números e mostra qual é ooo maior ou se são iguais
+Nathaly Ambrósio
+23/05/2023
+*/
+import java.util.Scanner;
+public class NathalyAmbrosio_exercicio9{
+        public static void main (String args []){ 
+         Scanner leitor = new Scanner (System.in);
+
+         //criando as variáveis
+         double n1, n2;
+         
+         //iniciando as variáveis
+         n1 = n2 = 0.0;
+         System.out.println(" insira dois valores");
+
+         //leia as variáveis
+        n1 = leitor.nextDouble();
+        n2 = leitor.nextDouble();
+
+        //comparação dos valores
+        if (n1 == n2)
+        System.out.println("os valores são iguais");
+
+        else if ( n1 > n2)
+                System.out.println("o primeiro valor é maior");
+                
+                else if (n1 < n2)
+                        System.out.println("o segundo número é maior");
+
+                        leitor.close();
+        }
+}
